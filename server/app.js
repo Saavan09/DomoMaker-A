974734b66ca3b3972f996bf9c188888e4a563d93
Saavan Tandon <st6041@rit.edu>
2025-11-08 14:51:17 -0500

    added session management for domomaker b

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const favicon = require('serve-favicon');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet'); // obscures info from malicious hackers
+const session = require('express-session'); // sessions for a user
 
 const router = require('./router.js');
 
@@ -23,12 +24,21 @@ app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
+
+// session configuration
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(session({
+  key: 'sessionid', // name of cookie so it can be tracked in requests
+  secret: 'Domo Arigato', // used as seed for hashing/creating unique session keys
+  resave: false, // only send it back to the database if it changes (saves unnecessary requests)
+  saveUninitialized: false,
+}));
 
 router(app);
 
