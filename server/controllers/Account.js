86d1052b8030b31fc24fb61820a4ef16dc6fb13f
Saavan Tandon <st6041@rit.edu>
2025-11-08 00:42:12 -0500

    login and signup functions

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -14,11 +14,50 @@ const logout = (req, res) => {
 };
 
 const login = (req, res) => {
+    const username = `${req.body.username}`;
+    const pass = `${req.body.pass}`;
 
+    if(!username || !pass) {
+        return res.status(400).json({ error: 'All fields are required!' });
+    }
+
+    return(Account.authenticate(username, pass, (err, account) => {
+        if(err || !account) {
+            return res.status(401).json({ error: 'Wrong username or password!' });
+        }
+
+        return res.json({ redirect: '/maker' }); //redirects to main page if signed up correctly
+    }))
 };
 
-const signup = (req, res) => {
+const signup = async(req, res) => {
+    //first, validate the data
+    const username = `${req.body.username}`;
+    const pass = `${req.body.pass}`;
+    const pass2 = `${req.body.pass2}`;
 
+    if(!username || !pass || !pass2) {
+        return res.status(400).json({ error: 'All fields are required!' });
+    }
+
+    if(pass !== pass2) {
+        return res.status(400).json({ error: 'Passwords do not match!' });
+    }
+
+    //attempt to hash the password
+    try {
+        const hash = await Account.generateHash(pass);
+        const newAccount = new Account({username, password: hash});
+        await newAccount.save();
+        return res.json({redirect: '/maker' });
+    }
+    catch(err) {
+        console.log(err);
+        if(err.code == 11000) {
+            return res.status(400).json({ error: 'Username already in use!' });
+        }
+        return res.status(500).json({ error: 'An error occured!' });
+    }
 };
 
 
